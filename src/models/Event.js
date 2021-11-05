0796596c6e1b7d338006a9bbb31bb500cf09f123
irlaandrade <paula.irla@gmail.com>
2021-11-04 22:41:54 -0300

    feat: add project models

---
+++ src/models/Event.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+
+const eventSchema = new Schema({
+  title: { type: String, required: true },
+  date: { type: String, required: true },
+  description: { type: String, required: true },
+  hour: { type: String, required: true },
+  local: { type: Object, required: true },
+  price: { type: String, required: true }
+}, { timestamps: true })
+
+const Event = mongoose.model('Event', eventSchema)
+
+module.exports = Event
